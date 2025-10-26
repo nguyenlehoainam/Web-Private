cd6c77636375609068aa7c6eb9703df880d981db
nguyenlehoainam <namngyenhoai21@gmail.com>
2025-10-27 02:22:18 +0700

    homework week 2

---
+++ Week1/ex1.js
@@ -0,0 +1,11 @@
+const button = document.querySelector("button");
+const handleClick = () => {
+  if (button.innerText === "Click me 🍔") {
+    button.innerText = "Ah i got clicked 🌭";
+    button.classList.add("click");
+  } else {
+    button.innerText = "Click me 🍔";
+    button.classList.remove("click");
+  }
+};
+button.addEventListener("click", handleClick);
